227a91e669e4f27620f8b639be44b8c81c036499
nicholasvilliers <47583787+nicholasvilliers@users.noreply.github.com>
2019-09-24 09:56:13 +1000

    Some main pages
    Added some but not all main pages, dynamic service page working. lots of stylying to be done still

--- ACTYouthServices/ACTYouthServicesWeb/Controllers/HomeController.cs
+++ ACTYouthServices/ACTYouthServicesWeb/Controllers/HomeController.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using ACTYouthServicesWeb.Models;
 
 namespace ACTYouthServicesWeb.Controllers
 {
     public class HomeController : Controller
     {
+        /*import database*/
+        private ACTYouthServicesDatabaseEntities db = new ACTYouthServicesDatabaseEntities();
+
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult ServicesMenu()
+        {
+            ViewBag.Message = "Services page.";
+
             return View();
         }
 
@@ -25,6 +39,59 @@ namespace ACTYouthServicesWeb.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        /* GET: Services/Details/N - dynamic service page*/
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+        /*Get Service types page*/
+        public ActionResult Type()
+        {
+            ViewBag.Message = "Service types page.";
+
+            return View();
+        }
+        /*Get Service Locations page*/
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Service locations page.";
+
+            return View();
+        }
+        /*----------------------------------------------------------*/
+        /*Service Locations-----------------------------------------*/
+        /*----------------------------------------------------------*/
+        public ActionResult Belconnen()
+        {
+            {
+                return View(db.Services.ToList());
+            }
+        }
+        /*----------------------------------------------------------*/
+        /*Service Categories----------------------------------------*/
+        /*----------------------------------------------------------*/
+        public ActionResult Shelter()
+        {
+            {
+                return View(db.Services.ToList());
+            }
+        }
+        public ActionResult Meals()
+        {
+            {
+                return View(db.Services.ToList());
+            }
         }
     }
 }
